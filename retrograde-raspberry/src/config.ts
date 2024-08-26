29d512cee660d768a984efab2709deaba5d6e563
xAmiBa <aminaba666@gmail.com>
2024-08-26 11:58:35 +0100

    astro yml

--- retrograde-raspberry/src/config.ts
+++ retrograde-raspberry/src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://aminaba.com", // replace this with your deployed domain
+  website: "https://xamiba.github.io", // replace this with your deployed domain
   author: "Amina Ba",
   profile: "https://github.com/XAmiBa",
   desc: "Backend Software Engineer",
--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://xamiba.github.io", // replace this with your deployed domain
   author: "Amina Ba",
   profile: "https://github.com/xAmiBa/",
   desc: "Amina Ba - Backend Software Engineer",
